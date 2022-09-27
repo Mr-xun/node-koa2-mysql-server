c59a6a39f9b0b6a394abedfcfec8fbaf42b7fd02
荀潇 <17810204418@163.com>
2022-09-27 17:29:32 +0800

    up

--- models/SystemRole.js
+++ models/SystemRole.js
@@ -2,7 +2,7 @@
  * @Author: xunxiao 17810204418@163.com
  * @Date: 2022-09-17 16:51:34
  * @LastEditors: xunxiao
- * @LastEditTime: 2022-09-27 17:14:23
+ * @LastEditTime: 2022-09-27 17:26:16
  * @Description: System_RoleModel
  */
 
@@ -24,12 +24,15 @@ const Role = DB.sequelize.define("system_role", {
         comment: "关联菜单",
         field: "menu_ids",
     },
-    isDelete: {
-        //是否删除 1是 0 否
-        type: Sequelize.INTEGER,
-        defaultValue: 0,
-        comment: "是否删除",
-        field: "is_delete",
+    createTime: {
+        type: Sequelize.DATE,
+        comment: "创建时间",
+        field: "create_time",
+    },
+    updateTime: {
+        type: Sequelize.DATE,
+        comment: "更新时间",
+        field: "update_time",
     },
 });
 Role.sync();
--- service/system/RoleService.js
+++ service/system/RoleService.js
@@ -2,7 +2,7 @@
  * @Author: xunxiao 17810204418@163.com
  * @Date: 2022-09-17 17:00:56
  * @LastEditors: xunxiao
- * @LastEditTime: 2022-09-27 16:55:09
+ * @LastEditTime: 2022-09-27 17:28:45
  * @Description: SystemRoleService
  */
 import SystemRole from "@root/models/SystemRole";
@@ -23,7 +23,7 @@ const BatchDel = async (ids) => {
 
 //获取单个角色
 const GetOne = async (where) => {
-    return SystemRole.findOne({ where, raw: true, attributes: { exclude: ["is_delete"] } });
+    return SystemRole.scope("bh").findOne({ where, raw: true});
 };
 //角色列表
 const GetAll = () => {
@@ -36,13 +36,12 @@ const GetAll = () => {
 };
 //角色列表分页
 const GetListByPage = ({ limit, offset }) => {
-    return SystemRole.findAndCountAll({
+    return SystemRole.scope("bh").findAndCountAll({
         where: {
             is_delete: 0,
         },
         limit,
         offset,
-        attributes: { exclude: ["isDelete"] },
     });
 };
 export default {
